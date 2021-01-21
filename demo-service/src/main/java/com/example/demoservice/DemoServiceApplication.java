711239d6f5d85c9c7fd32b3acd89a7725324e50e
Saarthak Gupta <saarthakgupta08@gmail.com>
2021-01-21 17:13:20 +0530

    add files

---
+++ demo-service/src/main/java/com/example/demoservice/DemoServiceApplication.java
@@ -0,0 +1,16 @@
+package com.example.demoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class DemoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoServiceApplication.class, args);
+	}
+
+}
